9e4088695f9d8942e3bc2c51516777681f093af4
CobaltBru <97279958+CobaltBru@users.noreply.github.com>
2025-03-19 11:15:08 +0900

    char

--- 250304_WinAPI/Character.cpp
+++ 250304_WinAPI/Character.cpp
@@ -21,7 +21,7 @@ Character::~Character()
 void Character::setPlayer(int p)//1, 2
 {
 	this->player = p;
-	screenWay = player == 1 ? 1 : -1;
+	screenWay = player == 1 ? true : false;
 }
 void Character::Init(vector<Image> images, FPOINT pos, float hp, float damage)
 {
@@ -39,70 +39,149 @@ void Character::Release()
 
 void Character::Update(float deltaTime)
 {
+	//키입력을 받을 수 있는 상태
 	if (currentState != STATE::PROCESS)
 	{
 		if (currentCommand == "A")
 		{
-			if (screenWay == 1)
+			if (screenWay == true) //뒷걸음질
 			{
-				currentState = STATE::BACK;
+				moveWay = -1;
+				speed = 0.7f;
+				if(currentState == STATE::DOWN)currentState = STATE::DOWNBACK;
+				else currentState = STATE::BACK;
 			}
-			else
+			else //앞으로 
 			{
-				currentState = STATE::WALK;
+				moveWay = 1;
+				speed = 1.0f;
+				if (currentState == STATE::DOWN)currentState = STATE::DOWNWALK;
+				else currentState = STATE::WALK;
 			}
 		}
 		else if (currentCommand == "D")
 		{
-			if (screenWay == 1)
+			if (screenWay == true) //앞으로
 			{
-				currentState = STATE::WALK;
+				moveWay = 1; 
+				speed = 1.0f;
+				if (currentState == STATE::DOWN)currentState = STATE::DOWNWALK;
+				else currentState = STATE::WALK;
 			}
-			else
+			else //뒷걸음질
 			{
-				currentState = STATE::BACK;
+				moveWay = -1;
+				speed = 0.7f;
+				if (currentState == STATE::DOWN)currentState = STATE::DOWNBACK;
+				else currentState = STATE::BACK;
 			}
 		}
-		else if (currentCommand == "S")
+		else if (currentCommand == "S") //숙이기
 		{
+			speed = 0;
 			currentState = STATE::DOWN;
 		}
-		else if (currentCommand == "AA")
+		else if (currentCommand == "AA") 
 		{
-			if (screenWay == 1)
+			if (screenWay == true) //백대쉬
 			{
+				speed = 3.0f;
+				moveWay = -1;
 				currentState = STATE::BACKDASH;
 			}
-			else
+			else //앞대쉬
 			{
+				speed = 3.0f;
+				moveWay = 1;
 				currentState = STATE::DASH;
 			}
 		}
 		else if (currentCommand == "DD")
 		{
-			if (screenWay == 1)
+			if (screenWay == true) //앞대쉬
 			{
+				speed = 3.0f;
+				moveWay = 1;
 				currentState = STATE::DASH;
 			}
-			else
+			else //백대쉬
 			{
+				speed = 3.0f;
+				moveWay = -1;
 				currentState = STATE::BACKDASH;
 			}
+		}
+		else
+		{
+			useSkill(currentCommand);
 		}
 
 	}
 
+	//동작중
+	if (currentState != STATE::IDLE)
+	{
+		timecnt += deltaTime;
+		framecnt = timecnt / (deltaTime); //현재 프레임 계산
+		if (currentState == STATE::PROCESS) //기술중
+		{
+			if (framecnt >= skillSet[currentSkill].maxFrame)//끝나면 IDLE로
+			{
+				endSkill();
+			}
+		}
+		else //일반동작
+		{
+			if (framecnt >= maxFrames[getIndex()])//루프처리
+			{
+				framecnt = 0;
+				timecnt = 0;
+			}
+		}
+	}
+	
+	
+	//기본상태일시 초기화
+	if (currentState == STATE::IDLE) 
+	{
+		timecnt == 0;
+		framecnt = 0;
+	}
+	
+	
 	Move(deltaTime);
 	
 }
 
 void Character::Render(HDC hdc)
 {
-	images[(int)currentState].Render(hdc, pos.x,pos.y);
+	if (currentState != STATE::IDLE)
+	{
+		if (currentState == STATE::PROCESS)
+		{
+			skillSet[currentSkill].image->Render(hdc, pos.x, pos.y, framecnt, screenWay);
+		}
+		else
+		{
+			images[getIndex()].Render(hdc, pos.x, pos.y, framecnt, screenWay);
+		}
+	}
 }
 
 void Character::Move(float deltaTime)
 {
+	if ((currentState == STATE::BACK) || (currentState == STATE::WALK))
+	{
+		pos.x += ((screenWay ? 1 : -1) * moveWay) * speed * deltaTime;
+	}
+	if ((currentState == STATE::DOWNBACK) || (currentState == STATE::DOWNWALK))
+	{
+		pos.x += ((screenWay ? 1 : -1) * moveWay) * speed * deltaTime * 0.5f;
+	}
+	if ((currentState == STATE::BACKDASH) || (currentState == STATE::DASH))
+	{
+		pos.x += ((screenWay ? 1 : -1) * moveWay) * speed * deltaTime * 3;
+	}
 }
 
 void Character::getCommand()
@@ -111,7 +190,77 @@ void Character::getCommand()
 
 void Character::updateCurrentScreenWay(Character* otherCharacter)
 {
-	if (pos.x - otherCharacter->pos.x < 0) screenWay = -1;
-	else if (pos.x - otherCharacter->pos.x > 0) screenWay = 1;
+	if (pos.x - otherCharacter->pos.x < 0) screenWay = false;
+	else if (pos.x - otherCharacter->pos.x > 0) screenWay = true;
 }
 
+void Character::useSkill(string str)
+{
+	for (int i = 0;i<skillSet.size();i++)
+	{
+		if (str.find(skillSet[i].command) != string::npos)
+		{
+			currentSkill = i;
+			currentState = STATE::PROCESS;
+
+			SKILL& skill = skillSet[currentSkill];
+			this->damage = skill.damage;
+			this->moveWay = skill.way;
+			this->speed = skill.speed;
+		}
+		else
+		{
+			currentState = STATE::IDLE;
+			return;
+		}
+	}
+	
+	
+}
+
+void Character::endSkill()
+{
+	currentState = STATE::IDLE;
+	this->damage = 0;
+	this->currentSpeed = 0;
+}
+
+
+int Character::getIndex()
+{
+	int idx = 0;
+	switch (currentState)
+	{
+	case STATE::IDLE:
+		idx = -1;
+		break;
+	case STATE::BACK:
+		idx = 0;
+		break;
+	case STATE::WALK:
+		idx = 1;
+		break;
+	case STATE::DOWN:
+		idx = 2;
+		break;
+	case STATE::DOWNBACK:
+		idx = 3;
+		break;
+	case STATE::DOWNWALK:
+		idx = 4;
+		break;
+	case STATE::DASH:
+		idx = 5;
+		break;
+	case STATE::BACKDASH:
+		idx = 6;
+		break;
+	case STATE::SKILL:
+		idx = -1;
+		break;
+	case STATE::PROCESS:
+		idx = -1;
+		break;
+	}
+	return idx;
+}
--- 250304_WinAPI/Character.h
+++ 250304_WinAPI/Character.h
@@ -8,25 +8,42 @@ private:
 	enum class STATE { IDLE, BACK, WALK, DOWN, DOWNBACK, DOWNWALK, DASH, BACKDASH,SKILL, PROCESS };
 	struct SKILL
 	{
-		string command;
-		Image image;
-		int maxFrame;
+		string command;		//커맨드
+		Image* image;		//실행 이미지
+		int maxFrame;		//총 프레임
+		int damage;			//데미지
+		int reach;			//사정거리
+		bool isUpperAttack;	//상단피격여부
+		bool isLowerAttack;	//하단피격여부
 
+		//현재 x축으로만 이동 가능
+		float startTime;
+		float endTime;
+		int way;			//이동방향
+		int speed;			//이동속도
 	};
 	int player;
 
 	FPOINT pos;
-	int screenWay;
-	int moveWay;
-	float speed;
+	bool screenWay; //화면에서 보고있는 방향
+	int moveWay; //이동방향
+	float characterSpeed;
+	float speed; //속도
 	float hp;
 	float currentHp;
 	float damage;
-	
-	string currentCommand;
 	vector<Image> images;
-	vector<SKILL> skillSet;
-	STATE currentState;
+	vector<int> maxFrames;
+	string currentCommand; //들어온 커맨드
+	
+	vector<SKILL> skillSet;//기술 목록
+	int currentSkill; //현재 재생중인 기술
+
+	int framecnt;
+	float timecnt;
+
+	STATE currentState; //상태
+
 public:
 	Character();
 	~Character();
@@ -38,6 +55,10 @@ public:
 	void getCommand();
 	void updateCurrentScreenWay(Character* otherCharacter);
 	void setPlayer(int p);
+	
+	void useSkill(string str);
+	void endSkill();
+	int getIndex();
 };
 
 
